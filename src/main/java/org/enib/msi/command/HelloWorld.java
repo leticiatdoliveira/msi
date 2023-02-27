43b5291d171f29fb39bea7471b16a9687912cc66
Leticia Tempone de Oliveira <leticiatdoliveira@unifei.edu.br>
2023-02-27 23:11:29 +0100

    Load in attributes and operations of UML in Modelio.

--- src/main/java/org/enib/msi/command/HelloWorld.java
+++ src/main/java/org/enib/msi/command/HelloWorld.java
@@ -9,6 +9,8 @@ import org.modelio.api.module.IModule;
 import org.modelio.api.module.command.DefaultModuleCommandHandler;
 import org.modelio.api.module.context.configuration.IModuleUserConfiguration;
 import org.modelio.api.module.context.log.ILogService;
+import org.modelio.metamodel.uml.statik.Attribute;
+import org.modelio.metamodel.uml.statik.Operation;
 import org.modelio.vcore.smkernel.mapi.MObject;
 
 /**
@@ -54,26 +56,47 @@ public class HelloWorld extends DefaultModuleCommandHandler {
         logService.info(sel.getName() + " / " + sel.getClass().getName());
        
         if(sel.getClass().getName().contains("Package")){
+
 	    	org.modelio.metamodel.uml.statik.Package p = (org.modelio.metamodel.uml.statik.Package)sel;
-	    	List<EObject>contents = p.eContents();
+	    	List<EObject> contents = p.eContents();
+
 	    	for(int i = 0; i < contents.size(); i++) {
 	    		EObject elem = contents.get(i);
+
 	    		if (elem.getClass().getName().contains("Class")) {
 	    			org.modelio.metamodel.uml.statik.Class c = (org.modelio.metamodel.uml.statik.Class)elem;
-	    			logService.info(c.getName());
-	    			
-	    			// busacando os atributos da classe
+	    			logService.info(c.getName()); // print the class name readed
+
+                    // Searching class attributes
 	    			List<Attribute> attributesCont = c.getOwnedAttribute();
-	    			for(int j = 0; i< attributesCont.size(); j++) {
-	    			
-	    
+	    			for(int j = 0; j<attributesCont.size(); j++) {
+                        Attribute attr = attributesCont.get(j);
+
+                        if(attr.getClass().getName().contains("Attribute")){
+                            org.modelio.metamodel.uml.statik.Attribute a =
+                                    (org.modelio.metamodel.uml.statik.Attribute)attr;
+                            logService.info(a.getName());
+                        }
 	    			}
 	    			
-	    			
-	    		}
-	    	}
-	    }   
-      
+	    			// Searching class operations
+	    			List<Operation> opContents = c.getOwnedOperation();
+	    			for(int i1=0; i1<opContents.size(); i1++) {
+                        Operation elem_op = opContents.get(i1);
+
+                        if(elem_op.getClass().getName().contains("Operation")){
+                            org.modelio.metamodel.uml.statik.Operation o =
+                                    (org.modelio.metamodel.uml.statik.Operation)elem_op;
+                            logService.info(o.getName());
+                        }
+
+                    }
+
+                }
+            }
+        }
     }
+      
 }
+
         
